b89b8cc809852f48c3247da301854c715191a493
hesitationIsDefeat <tanrioveronat@gmail.com>
2025-06-12 09:10:48 +0300

    Updated image resize logic

--- main.py
+++ main.py
@@ -16,18 +16,21 @@ from utils.read_write import read_waveform_data, save_spectrogram_image
 from utils.transform import (
     compute_spectrogram_raw,
     compute_spectrogram_labeled,
+    compute_global_db_stats,
     prepare_condition_array,
     compute_normalization_stats
 )
 
 TESTING_LIMIT: int = 5
 
-def main_preprocess(data_path: str, output_path: str, spec_height: int, spec_ext: str,
+def main_preprocess(data_path: str, output_path: str, spec_height: int, spec_width: int, spec_ext: str,
                     e_id_col: str, samp_fs_col: str, last_md_col: str,
                     stft_color_mode: str, is_testing: bool):
     df, waveforms = read_waveform_data(data_path=data_path, last_md_col=last_md_col)
     os.makedirs(output_path, exist_ok=True)
     iteration_amount: int = TESTING_LIMIT if is_testing else len(df)
+
+    global_db_min, global_db_max = compute_global_db_stats(data_path)
     for idx, row in tqdm(df.head(iteration_amount).iterrows(), total=iteration_amount, desc="Preprocess Progress"):
         e_id = row[e_id_col]
         fs = row[samp_fs_col]
@@ -35,7 +38,10 @@ def main_preprocess(data_path: str, output_path: str, spec_height: int, spec_ext
             waveform=waveforms[idx],
             samp_fs=fs,
             color_mode=stft_color_mode,
-            output_height=spec_height
+            output_height=spec_height,
+            output_width=spec_width,
+            global_db_min=global_db_min,
+            global_db_max=global_db_max
         )
         labeled_img = compute_spectrogram_labeled(img, times, freq, norm, stft_color_mode)
         save_spectrogram_image(img=img, e_id=e_id, output_dir=output_path, extension=spec_ext, file_name='raw', color_mode=stft_color_mode)
@@ -64,16 +70,15 @@ def main_train(data_path: str, spec_dir: str, spec_height: int, color_mode: str,
         depth_col=dep_col,
         mag_col=mag_col
     )
-    dataloader = DataLoader(dataset, batch_size=batch_size, pin_memory=True, shuffle=True, num_workers=4)
 
     device = torch.device(device_name)
 
     model = ConditionalUNet(in_channels=(3 if color_mode == 'color' else 1)).to(device)
     diffusion = Diffusion(device=device, timesteps=diff_ts, beta_start=diff_beta_start, beta_end=diff_beta_end)
 
-    train(model, diffusion, dataloader, epochs=epochs, lr=lr, model_save_path=model_save_path)
+    train(model, diffusion, dataset, epochs=epochs, batch_size=batch_size, lr=lr, model_save_path=model_save_path)
 
-def main_sample(data_path: str, model_path: str, spec_height: int, aspect_ratio_json_path: str, color_mode: str, spec_ext: str,
+def main_sample(data_path: str, model_path: str, spec_height: int, spec_width: int, color_mode: str, spec_ext: str,
                 num_samples: int, sample_save_dir: str, device_name: str,
                 e_id_col: str, lat_col: str, lon_col: str, dep_col: str, mag_col: str,
                 diff_ts: int, diff_beta_start: float, diff_beta_end: float):
@@ -88,22 +93,6 @@ def main_sample(data_path: str, model_path: str, spec_height: int, aspect_ratio_
 
     diffusion = Diffusion(device=device, timesteps=diff_ts, beta_start=diff_beta_start, beta_end=diff_beta_end)
 
-    print("alpha_prod shape:", diffusion.alpha_prod.shape)
-    print("betas shape:", diffusion.betas.shape)
-    print("First 10 alpha_prod:", diffusion.alpha_prod[:10])
-    print("First 10 betas:", diffusion.betas[:10])
-
-    if not os.path.exists(aspect_ratio_json_path):
-        raise FileNotFoundError(f"Aspect ratio JSON file not found at: {aspect_ratio_json_path}")
-
-    with open(aspect_ratio_json_path, 'r') as f:
-        ratio_data = json.load(f)
-
-    aspect_ratio = ratio_data.get("aspect_ratio", None)
-    if aspect_ratio is None:
-        raise ValueError("JSON file does not contain 'aspect_ratio' key.")
-
-    spec_width = int(round(spec_height * aspect_ratio))
     spec_dim = (spec_height, spec_width)  # (H, W)
 
     for idx, row in tqdm(df.head(num_samples).iterrows(), total=num_samples, desc="Sampling Progress"):
@@ -127,16 +116,10 @@ def main_sample(data_path: str, model_path: str, spec_height: int, aspect_ratio_
         else:
             img = img.squeeze(0)  # (1, H, W) → (H, W)
 
-        print(f"Sampled tensor min/max BEFORE denorm: {img.min().item()} / {img.max().item()}")
-
         # [-1, 1] to [0, 1]
         img = (img + 1.0) / 2.0
-        img = torch.clamp(img, 0, 1)
-        print(f"Sampled tensor min/max AFTER denorm: {img.min().item()} / {img.max().item()}")
-
-        img = (img * 255).round().to(torch.uint8).numpy()
-
-        print("Condition vector (normalized):", cond.cpu().numpy())
+        img = img.clamp(0, 1)
+        img = (img * 255).round().to(torch.uint8).cpu().numpy()
 
         save_spectrogram_image(
             img=img,
@@ -147,11 +130,12 @@ def main_sample(data_path: str, model_path: str, spec_height: int, aspect_ratio_
             color_mode=color_mode)
 
 SPEC_DIM_HEIGHT: int = 256
+SPEC_DIM_WIDTH: int = 326
 SPEC_EXTENSION: str = "png"
 IS_TESTING: bool = False
 COLOR_MODE: str = "color"
-BATCH_SIZE: int = 32
-LR: float = 5e-4
+BATCH_SIZE: int = 16
+LR: float = 1e-4
 EPOCHS: int = 100
 
 if __name__ == '__main__':
@@ -163,6 +147,7 @@ if __name__ == '__main__':
     p_pre.add_argument('--data-path', default="data/input/train/ew.csv")
     p_pre.add_argument('--output-path', default="data/output/spec/ew")
     p_pre.add_argument('--spec-dim-height', type=int, default=SPEC_DIM_HEIGHT)
+    p_pre.add_argument('--spec-dim-width', type=int, default=SPEC_DIM_WIDTH)
     p_pre.add_argument('--event-id-col', default="EventID")
     p_pre.add_argument('--sampling-fs-col', default="SamplingRate")
     p_pre.add_argument('--last-metadata-col', default="NumFreqSteps")
@@ -197,10 +182,11 @@ if __name__ == '__main__':
     p_sm.add_argument('--model-path', default="models/ddpm.pth")
     p_sm.add_argument('--spec-dir', default="data/output/spec/ew")
     p_sm.add_argument('--spec-dim-height', type=int, default=SPEC_DIM_HEIGHT)
+    p_sm.add_argument('--spec-dim-width', type=int, default=SPEC_DIM_WIDTH)
     p_sm.add_argument('--spec-aspect-ratio-path', type=str, default="spec_aspect_ratio.json")
     p_sm.add_argument('--spec-color-mode', choices=["grayscale", "color"], default=COLOR_MODE)
     p_sm.add_argument('--spec-ext', choices=["png"], default=SPEC_EXTENSION)
-    p_sm.add_argument('--num-samples', type=int, default=20)
+    p_sm.add_argument('--num-samples', type=int, default=100)
     p_sm.add_argument('--sample-save-dir', default="data/output/spec/ew")
     p_sm.add_argument('--device', default="cuda")
     p_sm.add_argument('--event-id-col', default="EventID", help='Name of the event id column')
@@ -216,7 +202,7 @@ if __name__ == '__main__':
 
     if args.command == 'preprocess':
         main_preprocess(
-            data_path=args.data_path, output_path=args.output_path, spec_height=args.spec_dim_height, spec_ext=args.spec_ext,
+            data_path=args.data_path, output_path=args.output_path, spec_height=args.spec_dim_height, spec_width=args.spec_dim_width, spec_ext=args.spec_ext,
             e_id_col=args.event_id_col, samp_fs_col=args.sampling_fs_col, last_md_col=args.last_metadata_col,
             stft_color_mode=args.spec_color_mode,
             is_testing=args.is_testing
@@ -231,7 +217,7 @@ if __name__ == '__main__':
         )
     elif args.command == 'sample':
         main_sample(
-            data_path=args.cond_csv, model_path=args.model_path, spec_height=args.spec_dim_height, aspect_ratio_json_path=args.spec_aspect_ratio_path, color_mode=args.spec_color_mode, spec_ext=args.spec_ext,
+            data_path=args.cond_csv, model_path=args.model_path, spec_height=args.spec_dim_height, spec_width=args.spec_dim_width, color_mode=args.spec_color_mode, spec_ext=args.spec_ext,
             num_samples=args.num_samples, sample_save_dir=args.sample_save_dir, device_name=args.device,
             e_id_col=args.event_id_col ,lat_col=args.lat_col, lon_col=args.lon_col, dep_col=args.dep_col, mag_col=args.mag_col,
             diff_ts=args.diff_ts, diff_beta_start=args.diff_beta_start, diff_beta_end=args.diff_beta_end
